0adc57a55a398f008276205ea1c23b5fc66d1f08
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2024-01-24 05:55:35 -0300

    chore: change project structure

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const { registerRootComponent } = require("expo");
+
+const { App } = require("./screens/Home");
+
+registerRootComponent(App);
---
+++ src/screens/Home.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export function App() {
+  return (
+    <View>
+      <Text>Hello World, that's works fine!</Text>
+    </View>
+  )
+}
+
